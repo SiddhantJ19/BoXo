006645e5b5b4c2ae118d0ca0a9a47474c42119af
Siddhant Jain <mail.siddhantjain19@gmail.com>
2019-04-22 23:03:13 +0530

    Starting again for good!

--- boxo/file_sharing/consumers.py
+++ boxo/file_sharing/consumers.py
@@ -65,16 +65,8 @@ class showDatabaseConsumer(JsonWebsocketConsumer):
         elif command == 'Search':
             filename = content['filename']
             get_fileinfo =  hashTable.objects.filter(filename__contains=filename).values('filename','filehash','size')
-            
-            # print(get_fileinfo)
-            # print([file['filehash'] for file in get_fileinfo])
-            # for file in get_fileinfo:
-            # get_peerinfo = refTable.objects.filter(filehash__filehash__in=[file['filehash'] for file in get_fileinfo]).values('filehash__filehash','peer')
-            # print(get_peerinfo)
+            print(get_fileinfo.query)
             get_peerinfo = refTable.objects.filter(filehash__filehash__in=[file['filehash'] for file in get_fileinfo]).values('filehash__filehash','filehash__size','peer__peer_ip')
-
-            # print(get_peerinfo)
-            # data = serializers.serialize('json', get_peerinfo)
             print(list(get_peerinfo))
             self.send_json({ 
                 "Peer_List" : list(get_peerinfo),
